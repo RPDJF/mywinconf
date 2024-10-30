ae2aa528fd1107b53f5009de205d512bd2f452de
rpdjf <ruipaulo.unify@outlook.fr>
2024-10-30 16:53:06 +0100

    Enable InstallerHashOverride in winget settings if requested

--- start.ps1
+++ start.ps1
@@ -7,6 +7,24 @@ Write-Host "-----------------------------------`n`n" -ForegroundColor Yellow
 Write-Host "Running scripts..." -ForegroundColor Yellow
 Write-Host "-----------------------------------`n" -ForegroundColor Yellow
 
+$response = Read-Host "enable InstallerHashOverride in winget settings? (y/n)"
+if ($response -eq "y") {
+	Write-Host "Enabling InstallerHashOverride..." -ForegroundColor Yellow
+	if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+	{
+		# Create a new process with elevated privileges
+		$newProcess = New-Object System.Diagnostics.ProcessStartInfo "powershell";
+		$newProcess.Arguments = "winget settings --enable InstallerHashOverride";
+		$newProcess.Verb = "runas";
+		[System.Diagnostics.Process]::Start($newProcess) | Out-Null;
+	}
+	else
+	{
+		winget settings --enable InstallerHashOverride
+	}
+	Write-Host "InstallerHashOverride enabled" -ForegroundColor Green
+}
+
 $response = Read-Host "Install CLI apps? (y/n)"
 if ($response -eq "y") {
 	Write-Host "Installing CLI apps..." -ForegroundColor Yellow
