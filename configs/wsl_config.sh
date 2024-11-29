877fe2d486aae48a80af93c420eef46c6bbab955
rpdjf <ruipaulo.unify@outlook.fr>
2024-11-29 12:30:40 +0100

    added fixes for wsl

--- configs/wsl_config.sh
+++ configs/wsl_config.sh
@@ -12,6 +12,15 @@ is_installed() {
     dpkg -l | grep -q "$1"
 }
 
+# Fixing 24.04 mess on WSL
+print_message "34" "Fixing 24.04 mess"
+if ! grep -q "^deb .*$ppa:oibaf/graphics-drivers" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    sudo add-apt-repository -y ppa:oibaf/graphics-drivers
+fi
+if ! grep -q "alias nautilus=" ~/.bashrc; then
+    echo "alias nautilus='dbus-launch nautilus'" >> ~/.bashrc
+fi
+
 # Update and upgrade apt
 print_message "34" "Updating apt..."
 sudo apt update -qq > /dev/null 2>&1
@@ -19,7 +28,7 @@ print_message "34" "Upgrading apt..."
 sudo apt upgrade -y -qq > /dev/null 2>&1
 
 # Install packages
-packages=("git" "make" "gcc" "g++" "python3")
+packages=("git" "make" "gcc" "g++" "python3" "nautilus" "wslu")
 
 print_message "34" "Installing packages..."
 for package in "${packages[@]}"; do
@@ -55,4 +64,37 @@ else
 	print_message "34" "Generating SSH key pair..."
 	ssh-keygen > /dev/null 2>&1
 fi
+
+# Add scripts for nautilus
+# Create the Nautilus script directory if it doesn't exist
+print_message "34" "Adding scripts for nautilus WSL"
+mkdir -p ~/.local/share/nautilus/scripts
+mkdir -p ~/.local/share/applications
+
+# Create the open_with_wslview.sh script
+cat << 'EOF' > ~/.local/share/nautilus/scripts/open_with_wslview.sh
+#!/bin/bash
+wslview "$1"
+EOF
+
+# Make the script executable
+chmod +x ~/.local/share/nautilus/scripts/open_with_wslview.sh
+
+# Create the .desktop file for the custom action
+cat << 'EOF' > ~/.local/share/applications/wslview.desktop
+[Desktop Entry]
+Name=Open with WSLView
+Exec=wslview %f
+Type=Application
+Terminal=false
+Icon=utilities-terminal
+MimeType=application/octet-stream;
+EOF
+
+# Update the MIME database
+update-desktop-database ~/.local/share/applications
+
+# Set the default application for files
+xdg-mime default wslview.desktop application/octet-stream
+
 cat ~/.ssh/id_rsa.pub
